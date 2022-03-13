715426a4f2cb71b7b88f6a83734d19bb1572fb72
Yukito-F <yukito.izuminohara@gmail.com>
2022-03-13 11:49:58 +0900

    2022_03_!3_update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     GameObject sphere;
     GameObject piller;
+
+    int turn = 1; // 1:white, -1:black
 
     int[,,] field = new int[4,4,4]; // 縦,奥,横
     void Start()
@@ -35,40 +35,126 @@ public class GameManager : MonoBehaviour
 
             if(Physics.Raycast(ray, out RaycastHit hit))
             {
+                
                 Vector3 temp = hit.transform.position;
                 temp.y = 10;
-                Instantiate(sphere, temp, Quaternion.identity);
 
+                int y = 0;
+                int x = (int)(temp.x + 1.5f);
+                int z = (int)(1.5f - temp.z);
+
+                if (field[3, x, z] == 0)
+                {
+                    var obj = Instantiate(sphere, temp, Quaternion.identity);
+                    if (turn == -1)
+                    {
+                        obj.GetComponent<Renderer>().material.color = Color.black;
+                    }
+                    while (field[y, x, z] != 0) y++;
+
+                    field[y, x, z] = turn;
+                    if(CheckField(y, x, z))
+                    {
+                        Debug.Log("Game End");
+                    }
+
+                    turn *= -1;
+                }
 
                 // --------------------Debug--------------------
-                FieldDebug();
+                // FieldDebug();
             }
             
         }
     }
 
+    bool CheckField(int y, int x, int z)
+    {
+        // 直線
+        int sumY = 0, sumX = 0, sumZ = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            sumY += field[i, x, z];
+            sumX += field[y, i, z];
+            sumZ += field[y, x, i];
+        }
+        if (Mathf.Abs(sumY) == 4 || Mathf.Abs(sumX) == 4 || Mathf.Abs(sumZ) == 4)
+        {
+            return true;
+        }
+
+        // 2次元的ナナメ（y固定）
+        int sumRX = 0, sumLX = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            sumRX += field[y, i, i    ];
+            sumLX += field[y, i, 3 - i];
+        }
+        if (Mathf.Abs(sumRX) == 4 || Mathf.Abs(sumLX) == 4)
+        {
+            return true;
+        }
+
+        // 2次元的ナナメ（x固定）
+        sumRX = 0; sumLX = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            sumRX += field[i,     x, i];
+            sumLX += field[3 - i, x, i];
+        }
+        if (Mathf.Abs(sumRX) == 4 || Mathf.Abs(sumLX) == 4)
+        {
+            return true;
+        }
+        
+        // 2次元的ナナメ（z固定）
+        sumRX = 0; sumLX = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            sumRX += field[i, i,     z];
+            sumLX += field[i, 3 - i, z];
+        }
+        if (Mathf.Abs(sumRX) == 4 || Mathf.Abs(sumLX) == 4)
+        {
+            return true;
+        }
+
+        // 3次元的ナナメ
+        int sumRFX = 0, sumRCX = 0, sumLFX = 0, sumLCX = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            sumRFX += field[i, i,     i];
+            sumRCX += field[i, 3 - i, 3 - i];
+            sumLFX += field[i, i,     3 - i];
+            sumLCX += field[i, 3 - i, i];
+        }
+        if (Mathf.Abs(sumRFX) == 4 || Mathf.Abs(sumRCX) == 4 || Mathf.Abs(sumLFX) == 4 || Mathf.Abs(sumLCX) == 4)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
     void FieldDebug()
     {
+        //string message = "";
+
+        string l1 = "";
+        string l2 = "";
+        string l3 = "";
+        string l4 = "";
+
         int index = 0;
-        string message = "";
         foreach (int item in field)
         {
-            if (index % 16 == 0)
-            {
-                Debug.Log("Layer " + index / 12);
-            }
-            message += item;
 
-            if (index % 16 < 15)
-            {
-                message += ",";
-            }
-            else
-            {
-                Debug.Log(message);
-                message = "";
-            }
             index++;
         }
+
+        Debug.Log(l1);
+        Debug.Log(l2);
+        Debug.Log(l3);
+        Debug.Log(l4);
     }
 }
